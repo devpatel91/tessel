9cc16421b7b1b663b3d4c361f91465ec0019f32c
Ten <tenloh@gmail.com>
2016-08-12 15:57:56 -0400

    working on png buffer

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 var express = require('express');
 var morgan = require('morgan');
+var qr_reader = require('./qr_reader');
+var http = require('http');
+var image;
+
+setTimeout(function(){
+  var options = {
+      hostname: 'http://tessel-02a38f3f910e.local:8080',
+      method: 'GET',
+    }
+  http.get('http://tessel-02a38f3f910e.local:8080/', function(response){
+    //http.request(options, function(response){
+      // response.setEncoding('base64'); //?
+      response.on('data', function(image){
+        console.log(image)
+        var decoded = qr_reader(image);
+        console.log(decoded);
+        //response.send(decoded);
+      })
+
+
+    // var options = {
+    //   hostname: 'http://localhost',
+    //   port: 5577,
+    //   path: '/',
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type':' image/jpg'
+    //   }
+    // }
+    // http.request(options, function(res){
+    //   res.write(image);
+    //   res.end()
+    // })
+  });
+}, 0);
+
 
 var app = express();
 
@@ -10,6 +46,14 @@ app.get('/', function(req, res, next){
   res.send('You have reached Ten\'s computer');
 })
 
+app.post('/', function(req, res, next){
+  var imagePng = res.body; //Get image from post
+  var decoded = qr_reader(imagePng);
+  console.log(decoded);
+  res.send(decoded);
+})
+
+
 app.use(function(err, req, res, next){
   res.send(err);
 })
@@ -17,3 +61,5 @@ app.use(function(err, req, res, next){
 app.listen(5577, function(){
   console.log('You are up running on port 5577');
 })
+
+
--- qr_reader.js
+++ qr_reader.js
@@ -2,19 +2,42 @@ var QrCode = require('qrcode-reader');
 var fs = require('fs');
 var PNG = require('png-js');
 
-
 var qr = new QrCode();
 
-//qr.callback = function(result,err) { if(result) console.log(result) }
 
-var c = fs.readFileSync(__dirname + '/qrcodeee.png');
-var p = new PNG(c);
+// var fileName = __dirname + '/qrcodeee.jpeg';
+// var c = fs.readFileSync(__dirname + '/qrcodeee.jpeg');
 
-p.decode(function(data) {
 
-        qr = new QrCode();
-        qr.callback = function(result) {
-            console.log(result);
-        }
-        qr.decode(p, data);
-    });
+// var p = new JPG(c);
+// pngToJpg({
+//   input: p,
+//   output: fileName.substring(0, fileName.indexOf('.')) + 'png',
+// }, function(){
+//   console.log('png')
+// })
+
+// p.decode(function(data) {
+
+//         qr = new QrCode();
+//         qr.callback = function(result) {
+//             console.log(result);
+//         }
+//         qr.decode(p, data);
+//     });
+
+
+var qr_reader = function(file){
+  //var c = fs.readFileSync(pngPath);
+  var p = new PNG(file);
+
+  p.decode(function(data) {
+      qr = new QrCode();
+      qr.callback = function(result) {
+           return result;
+      }
+      qr.decode(p, data);
+  });
+}
+ 
+module.exports = qr_reader;
